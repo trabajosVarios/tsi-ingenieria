bce96c6ca897969354897bcd2ecb6994c827d5e8
Mauricio Aguila G <prototipo00@gmail.com>
2018-03-27 01:04:47 -0300

    Listos para subida preliminar

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,10 +16,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$to = $email_address; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Contacto desde Sitio Web TSI Ingenieria Spa:    $name";
+$email_body = "Has recivido un nuevo mensaje desde la pagina Inves Ing Spa \n\n"."Con los siguientes detalles:\n\nNombres de contacto: $name\n\nEmail: $email_address\n\nTeléfono de Contacto: $phone\n\nMensaje:\n$message";
+$headers = "From: contacto@ingenieria-tsi.cl\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
